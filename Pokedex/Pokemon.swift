c0099e50b15050f1ad5455d0035afa96a0e08a8b
Eric Sirinian <e,v,sirinian@gmail.com>
2017-01-11 22:45:29 -0500

    Created base Pokemon class

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,34 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Eric Sirinian on 1/11/17.
+//  Copyright © 2017 Eric Sirinian. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexID: Int!
+    
+    var name: String {
+        if _name == nil {
+            _name = ""
+        }
+        return _name
+    }
+    
+    var pokedexID: Int {
+        if _pokedexID == nil {
+            _pokedexID = 0
+        }
+        return _pokedexID
+    }
+    
+    init(name: String, pokedexID: Int) {
+        self._name = name
+        self._pokedexID = pokedexID
+    }
+}
